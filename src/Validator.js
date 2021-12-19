cf14737d6db1315d1a8de38052690b4f82973b4c
Zoe Peterson <zoe.peterson@generalassemb.ly>
2021-12-19 10:05:11 -0700

    Update Validator.js

--- src/Validator.js
+++ src/Validator.js
@@ -6,19 +6,19 @@ function Validator () {
       <div className="form">
         <h1>Sign Up</h1>
         <form>
-          <input type="text" placeholder="Username" id="username" />
           <label htmlFor="username">Username</label>
+          <input type="text" placeholder="Username" id="username" />
 
-          <input type="password" placeholder="Password" id="password" />
           <label htmlFor="password">Password</label>
-
+          <input type="password" placeholder="Password" id="password" />
+          
+          <label htmlFor="passwordConfirm">Confirm password</label>
           <input
             type="password"
             placeholder="Confirm password"
             id="passwordConfirm"
           />
-          <label htmlFor="passwordConfirm">Confirm password</label>
-
+          
           <button type="submit">Sign Up</button>
           <p>Passwords must match.</p>
         </form>

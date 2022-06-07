b2bd166239c4c6314dc295990f85de4a8f87812c
Esin Saribudak <53010153+esin87@users.noreply.github.com>
2022-06-07 15:58:00 -0500

    Fix markup so that styles work correctly

--- src/Validator.js
+++ src/Validator.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import './Validator.css';
 
-function Validator () {
-    return (
-      <div className="form">
-        <h1>Sign Up</h1>
-        <form>
-          <label htmlFor="username">Username</label>
-          <input type="text" placeholder="Username" id="username" />
+function Validator() {
+	return (
+		<div className='form'>
+			<h1>Sign Up</h1>
+			<form>
+				<input type='text' placeholder='Username' id='username' />
+				<label htmlFor='username'>Username</label>
 
-          <label htmlFor="password">Password</label>
-          <input type="password" placeholder="Password" id="password" />
-          
-          <label htmlFor="passwordConfirm">Confirm password</label>
-          <input
-            type="password"
-            placeholder="Confirm password"
-            id="passwordConfirm"
-          />
-          
-          <button type="submit">Sign Up</button>
-          <p>Passwords must match.</p>
-        </form>
-      </div>
-    );
+				<input type='password' placeholder='Password' id='password' />
+				<label htmlFor='password'>Password</label>
+
+				<input
+					type='password'
+					placeholder='Confirm password'
+					id='passwordConfirm'
+				/>
+				<label htmlFor='passwordConfirm'>Confirm password </label>
+
+				<button type='submit'>Sign Up</button>
+				<p>Passwords must match.</p>
+			</form>
+		</div>
+	);
 }
 
 export default Validator;
